84ea37945a6544842e635f117ae70da74870af97
Uday <udaydhir794@gmail.com>
2025-10-01 12:07:07 +0530

    created LoginForm component

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,10 @@
 import "./App.css";
+import LoginForm from "./components/LoginForm/LoginForm";
 
 function App() {
-  return <></>;
+  return <>
+  <LoginForm />
+  </>;
 }
 
 export default App;
---
+++ FrontEnd/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./LoginForm.css";
+
+function LoginForm() {
+  return (
+    <>
+        <form className="loginForm">
+          <label htmlFor="email">
+            <input type="email" name="email" id="email" placeholder="Email" />
+          </label>
+          <label htmlFor="password">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+            />
+          </label>
+          <label className="loginOptions">
+            <input type="checkbox" name="rememberMe" id="rememberMe" /> Remember
+            me
+          </label>
+          <a href="#" className="loginOptions">
+            Forgot password?
+          </a>
+          <button className="loginButton" type="submit">
+            Login
+          </button>
+        </form>
+    </>
+  );
+}
+
+export default LoginForm;
